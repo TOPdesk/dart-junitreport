1e11b0a2cdaf5abb3cf23f96d34004517c03733f
Roel Spilker <r.spilker@gmail.com>
2017-09-21 00:27:22 +0200

    Updated dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,12 @@
+## 0.3.0
+* updated dependencies:
+  * sdk: '>=1.19.0 <2.0.0'
+  * xml: '^2.6.0'
+  * intl: '^0.15.0'
+  * testreport: '^0.3.0'
+* removed transitive dependencies on 'build_collection' and 'quiver'
+
+
 ## 0.2.1
 * restrict dependency on xml to '>= 2.5.0 <2.6.0' because the XmlPrettyWriter moved to a different library
 * use one system-out element to contain all outputs instead of one per output
--- lib/src/impl/xml.dart
+++ lib/src/impl/xml.dart
@@ -1,8 +1,9 @@
-// Copyright (c) 2016, TOPdesk. Please see the AUTHORS file for details.
+// Copyright (c) 2016-2017, TOPdesk. Please see the AUTHORS file for details.
 // All rights reserved. Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
 import 'package:xml/xml.dart';
+import 'package:xml/xml/visitors/pretty_writer.dart';
 
 XmlDocument doc(Iterable<XmlNode> children) {
   return new XmlDocument([
