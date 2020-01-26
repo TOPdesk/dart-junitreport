bce7226042f87d7ca975435bf8609e60367afa23
Roel Spilker <r.spilker@gmail.com>
2020-01-26 17:47:44 +0100

    add documentation

--- lib/src/api/junitreport.dart
+++ lib/src/api/junitreport.dart
@@ -1,13 +1,21 @@
-// Copyright (c) 2016-2019, TOPdesk. Please see the AUTHORS file for details.
+// Copyright (c) 2016-2020, TOPdesk. Please see the AUTHORS file for details.
 // All rights reserved. Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
 import 'package:testreport/testreport.dart';
 import 'package:junitreport/src/impl/report.dart';
 
+/// Transformer to convert a [Report] to an xml String.
 abstract class JUnitReport {
+  /// Creates a JUnitReport with the given [base] and [package].
+  ///
+  /// In the [Report], the [Suite]s contain the path to the test files.
+  ///
+  /// To modify this path, [base] will be removed from it,
+  /// and [package] will be prepended.
   factory JUnitReport({String base, String package}) =>
       XmlReport(base, package);
 
+  /// Transforms the given [Reports] to an xml String.
   String toXml(Report report);
 }
